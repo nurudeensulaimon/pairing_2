5002ef2ac9fdd4de0b9f0f48e4c2e033ca5579c5
Nurudeen Sulaimon <nerodean@Neros-MacBook-Pro.local>
2023-06-22 18:21:43 -0400

    If statements for plant living

---
+++ plants.rb
@@ -0,0 +1,11 @@
+gets_sunshine = false
+
+ounces_of_water = 2
+
+if gets_sunshine == true && ounces_of_water >= 10
+    puts "plants will live"
+elsif      
+     gets_sunshine ==false || ounces_of_water < 10
+     puts "plants will die"
+end 
+
